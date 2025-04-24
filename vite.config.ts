7fccb03502abc529e593e8888da614fd8d0e110b
shwetankagarwal <shwetank2003@gmail.com>
2025-04-24 16:08:19 +0530

    Update project configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    plugins: [react()],
+    base: "/Veritas-Careers-Application-Portal",
+    optimizeDeps: {
+        exclude: ["lucide-react"],
+    },
 });
